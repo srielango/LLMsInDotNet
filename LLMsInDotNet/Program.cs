2cb0032c7ea183a61dec4b06a101c500f0ad973d
srielango <srielango@gmail.com>
2025-02-28 19:37:58 +0530

    Add project files.

---
+++ LLMsInDotNet/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder();
+builder.Services.AddChatClient(new OllamaChatClient(new Uri("http://localhost:11434"), "llama3"));
+
+var app = builder.Build();
+
+var chatClient = app.Services.GetRequiredService<IChatClient>();
+
+var chatCompletion = await chatClient.GetResponseAsync("What is Docker?");
+
+Console.WriteLine(chatCompletion.Message.Text);
+
