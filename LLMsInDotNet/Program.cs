f7519cdb3e1a658d22d0f1f93d2d05913dc15f06
srielango <srielango@gmail.com>
2025-03-13 21:11:36 +0530

    Added chat history

--- LLMsInDotNet/Program.cs
+++ LLMsInDotNet/Program.cs
@@ -1,18 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-
-using Microsoft.Extensions.AI;
+﻿using Microsoft.Extensions.AI;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
+///https://www.milanjovanovic.tech/blog/working-with-llms-in-dotnet-using-microsoft-extensions-ai
+///To run ollama docker image in a non GPU machine use the command below
+///docker run -d -v ollama_data:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+///Note that the response is slow when there is no GPU
 
 var builder = Host.CreateApplicationBuilder();
 builder.Services.AddChatClient(new OllamaChatClient(new Uri("http://localhost:11434"), "llama3"));
 
 var app = builder.Build();
 
+var chatHistory = new List<ChatMessage>();
+
 var chatClient = app.Services.GetRequiredService<IChatClient>();
 
-var chatCompletion = await chatClient.GetResponseAsync("What is Docker?");
+Console.WriteLine("Conntected to LLM");
 
-Console.WriteLine(chatCompletion.Message.Text);
+while (true)
+{
+    Console.Write("Your prompt: ");
+    var userPrompt = Console.ReadLine();
+    if(userPrompt.Trim().ToUpper() == "EXIT")
+    {
+        break;
+    }
+    chatHistory.Add(new ChatMessage(ChatRole.User, userPrompt));
+
+    Console.Write("AI: ");
+    var chatResponse = "";
+    await foreach(var item in chatClient.GetStreamingResponseAsync(chatHistory))
+    {
+        Console.Write(item.Text);
+        chatResponse += item.Text;
+    }
+    chatHistory.Add(new ChatMessage(ChatRole.Assistant, chatResponse));
+    Console.WriteLine();
+}
+
+//var chatCompletion = await chatClient.GetResponseAsync("What is Docker?  Explain in 100 words");
+
+//Console.WriteLine(chatCompletion.Message.Text);
 
